7a9d20af16e0283ba633f4e988ce8ed5c3a04dfb
Lucas <lucasdraichi@gmail.com>
2020-09-07 14:47:23 -0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -31,7 +31,7 @@ Vue.use(Router)
 const router = new Router({
   mode: 'history',
   routes: paths.map(path => route(path.path, path.view, path.name)).concat([
-    { path: '*', redirect: '/draichiboard' }
+    { path: '*', redirect: '/draichiboard-old' }
   ]),
   scrollBehavior (to, from, savedPosition) {
     if (savedPosition) {
